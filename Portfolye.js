5a369d8255bfba21064afcb9e9446070fe3b7f97
HasanSariyildiz-bit <Hasan.Sariyildiz@my-aolcc.com>
2024-07-18 21:27:33 +0300

    Delete Portfolye.js

--- Portfolye.js
+++
@@ -1,6 +0,0 @@
-// Simple JavaScript for a dynamic message on the website
-document.addEventListener("DOMContentLoaded", function() {
-    const dynamicElement = document.createElement('p');
-    dynamicElement.textContent = "Welcome to my portfolio!";
-    document.body.insertBefore(dynamicElement, document.body.firstChild);
-});
